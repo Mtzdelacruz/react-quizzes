08339d260f68687a153e5369dc2b46888f90c605
Cristina Lizbeth Martínez de la Cruz <itsirc_09@hotmail.com>
2023-10-08 06:46:05 -0600

    react-project01

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
 import './App.css';
 
+import Avatar from './avatar/avatar';
 import Badge from "./badge/badge";
 import Card from './card/card';
 import Footer from './footer/footer';
 import Header from './header/header';
+import nocheEstrellada from "./nocheEstrellada.jpg"
 
 function App() {
   return (
     <div className="App">
       <Header title= "React Beginner's Quiz"/>
-      <Card title= "Funciones" content ="Componente básico de react" children="Especificaciones" />
-  
-      <Badge label= "New" color='sucess'/>
-      <Footer content= "Created with ❤️ by Cristina"/>
-      
 
+      
+    <div className="card-container">
+      <Card title= "Card 1" content ="Insert content 1" />
+      <Card title= "Card 2" content= "Insert content 2" />
+      <Card title= "Card 3" content= "Insert content 3" />
+
+      <Badge label= "New" color='sucess'/>
+
+      <Avatar 
+      imageSrc= {nocheEstrellada}
+      description= "This is my Avatar" />
+    </div>
+
+
+      <Footer content= "Created with ❤️ by Cristina"/>
     </div> 
   );
 }
